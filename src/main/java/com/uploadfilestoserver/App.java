18b49834b8759189f480f6f971bc890eb4a47e54
Cristian <cristian.dugacicu@ubitricity.com>
2017-11-10 20:22:51 +0100

    works with uploading multiple files

--- src/main/java/com/uploadfilestoserver/App.java
+++ src/main/java/com/uploadfilestoserver/App.java
@@ -10,92 +10,108 @@ import resources.Properties;
 import retrofit2.*;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class App {
 
     private static final String API_BASE_URL = Properties.API_BASE_URL;
     private static final String X_API_TOKEN = Properties.X_API_TOKEN;
     private static final String ACCESS_TOKEN = Properties.ACCESS_TOKEN;
+    private static final String FILE1 = "src/main/java/resources/dummy1.txt";
+    private static final String FILE2 = "src/main/java/resources/dummy2.txt";
 
 
-    public static void main( String[] args ) {
-        System.out.println( "started!" );
+    public static void main( String[] args ) throws InterruptedException {
+        System.out.println( "App started!" );
 
+//        callGetPublicSsos();
+//        Thread.sleep(4000L);
+//        System.out.println(".");
+//
+//        callGetprofile();
+//        Thread.sleep(4000L);
+//        System.out.println(".");
+//
+        callCreateSupport();
+        Thread.sleep(4000L);
+        System.out.println(".");
+    }
+
+    public static void callGetPublicSsos() {
         Retrofit retrofit = HttpClient.getServerOneClient(API_BASE_URL);
-
 
         // Create a very simple REST adapter which points the GitHub API endpoint.
         ServerOne client =  retrofit.create(ServerOne.class);
 
-//
-//
-//
-//
-//        // Fetch public ssos
-//        Call<String> callSsos = client.getPublicSsos(X_API_TOKEN);
-//
-//        // Execute the call asynchronously. Get a positive or negative callback.
-//        callSsos.enqueue(new Callback<String>() {
-//            @Override
-//            public void onResponse(Call<String> call, Response<String> response) {
-//                // The network call was a success and we got a response
-//                System.out.println(response);
-//                System.out.println(response.body());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<String> call, Throwable t) {
-//                // the network call was a failure
-//                t.printStackTrace();
-//            }
-//        });
-//
-//
-//
-//
-//
-//
-//
-//        // Fetch private profile.
-//        Call<String> callProfile = client.getprofile(ACCESS_TOKEN, X_API_TOKEN);
-//
-//        // Execute the call asynchronously. Get a positive or negative callback.
-//        callProfile.enqueue(new Callback<String>() {
-//            @Override
-//            public void onResponse(Call<String> call, Response<String> response) {
-//                // The network call was a success and we got a response
-//                System.out.println(response);
-//                System.out.println(response.body());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<String> call, Throwable t) {
-//                // the network call was a failure
-//                t.printStackTrace();
-//            }
-//        });
 
+        // Fetch public ssos
+        Call<String> callSsos = client.getPublicSsos(X_API_TOKEN);
 
-        uploadFile();
+        // Execute the call asynchronously. Get a positive or negative callback.
+        callSsos.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                // The network call was a success and we got a response
+                System.out.println(response);
+                System.out.println(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                // the network call was a failure
+                t.printStackTrace();
+            }
+        });
     }
 
 
-    public static void uploadFile() {
+    public static void callGetprofile() {
+        Retrofit retrofit = HttpClient.getServerOneClient(API_BASE_URL);
+
+        // Create a very simple REST adapter which points the GitHub API endpoint.
+        ServerOne client =  retrofit.create(ServerOne.class);
+
+
+        // Fetch private profile.
+        Call<String> callProfile = client.getprofile(ACCESS_TOKEN, X_API_TOKEN);
+
+        // Execute the call asynchronously. Get a positive or negative callback.
+        callProfile.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                // The network call was a success and we got a response
+                System.out.println(response);
+                System.out.println(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                // the network call was a failure
+                t.printStackTrace();
+            }
+        });
+    }
+
+
+    public static void callCreateSupport() {
         // create upload service client
         ServerOne service = ServiceGenerator.createService(ServerOne.class);
 
         // use the FileUtils to get the actual file by uri
-        File file = FileUtils.getFile("src/main/java/resources/dummy.txt");
+        File file1 = FileUtils.getFile(FILE1);
+        File file2 = FileUtils.getFile(FILE2);
 
         // create RequestBody instance from file
-        RequestBody requestFile = RequestBody.create(
-                        MediaType.parse("image/png"),
-                        file
-                    );
+        RequestBody requestFile1 = RequestBody.create(MediaType.parse("image/png"), file1);
+        RequestBody requestFile2 = RequestBody.create(MediaType.parse("image/png"), file2);
 
         // MultipartBody.Part is used to send also the actual file name
-        MultipartBody.Part supportFiles =
-                MultipartBody.Part.createFormData(file.getName(), file.getName(), requestFile);
+        // ! An empty list would end in a sent email with no attachments.
+        List<MultipartBody.Part> supportFiles = Arrays.asList(
+                MultipartBody.Part.createFormData("supportFiles", file1.getName(), requestFile1),
+                MultipartBody.Part.createFormData("supportFiles", file2.getName(), requestFile2)
+        );
 
 //        RequestBody description = RequestBody.create(MultipartBody.FORM, "test from Cristian");
 
@@ -107,8 +123,8 @@ public class App {
             @Override
             public void onResponse(Call<ResponseBody> call,
                                    Response<ResponseBody> response) {
-                System.out.println(response.body());
                 System.out.println(response);
+                System.out.println(response.body());
             }
 
             @Override
--- src/main/java/com/uploadfilestoserver/RemoteServer/ServerOne.java
+++ src/main/java/com/uploadfilestoserver/RemoteServer/ServerOne.java
@@ -1,17 +1,18 @@
 package com.uploadfilestoserver.RemoteServer;
 
 import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.*;
 
+import java.util.List;
+
 public interface ServerOne {
-//    @GET("v1/external_ssos/search?accessToken=public&action=index&controller=public&locale=en")
-//    Call<String> getPublicSsos(
-//            @Header("X-API-TOKEN") String xApiToken
-//    );
-//
+    @GET("v1/external_ssos/search?accessToken=public&action=index&controller=public&locale=en")
+    Call<String> getPublicSsos(
+            @Header("X-API-TOKEN") String xApiToken
+    );
+
     @POST("v1/clients/profile")
     Call<String> getprofile(
             @Query("accessToken") String accessToken,
@@ -19,14 +20,14 @@ public interface ServerOne {
     );
 
     @Multipart
-    @POST("portal-mediator/services/v1/clients/profile/support")
-//    @POST("v1/clients/profile/support")
+//    @POST("portal-mediator/services/v1/clients/profile/support")
+    @POST("v1/clients/profile/support")
     Call<ResponseBody> createSupport(
             @Query("accessToken") String accessToken,
             @Header("X-API-TOKEN") String xApiToken,
             @Header("Content-Type") String contentType,
             @Part("supportBody") String supportBody,
             @Part("supportType") String supportType,
-            @Part MultipartBody.Part supportFiles
+            @Part List<MultipartBody.Part> supportFiles
     );
 }
